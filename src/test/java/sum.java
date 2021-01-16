74abf9640c56ab350580362b81c393fb679149df
reddypernati <reddypernati@gmail.com>
2021-01-16 11:47:15 -0500

    revert sum.java

--- src/test/java/sum.java
+++ src/test/java/sum.java
@@ -1,4 +1,4 @@
-
+package arrays;
 
 public class sum{
 	
@@ -16,20 +16,3 @@ public class sum{
 	      System.out.println("Sum of array elements is:"+sum);
 	   }
 	}
-
-	/*//declaring array
-		int arr1[] = {12,13,14,44};
-		int total=0;
-		
-		//traversing the array with for-each loop 
-		for(int i:arr) {
-			
-			total = total + i;
-		}
-		System.out.println("Total: "+ total);
-		}
-		}*/
-		
-		
-		//The syntax of Java for-each loop consists of data_type with the variable
-		//followed by a colon (:), then array or collection.
